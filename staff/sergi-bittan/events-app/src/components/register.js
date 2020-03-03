80c7a005f8b975486f101e51fa2349dcdf9b1234
Sergi <sergibittangonzalez@MacBook-Pro-de-Sergi.local>
2020-03-04 00:02:48 +0100

    starting React/hooks

---
+++ staff/sergi-bittan/events-app/src/components/register.js
@@ -0,0 +1,37 @@
+import React from "react"
+export default
+
+function Register() {
+    return <form className="register" onSubmit={event => {
+        event.preventDefault()
+
+        const name = event.target.name.value
+        const surname = event.target.surname.value
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        onRegister(name, surname, email, password)
+
+    }}>
+         <h2>Sing-in</h2>
+
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="surname" placeholder="surname" />
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button>Register</button>
+        <a href="" onClick={event => {
+            event.preventDefault()
+
+            onToLogin()
+        }}>Login</a>
+    </form>
+
+}
+
+
+
+
+
+
+
