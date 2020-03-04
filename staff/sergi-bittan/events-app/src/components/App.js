a281500fddd9dd7c078cd41208de635b86e3c1d8
Sergi <sergibittangonzalez@MacBook-Pro-de-Sergi.local>
2020-03-04 13:35:07 +0100

    trying styles

--- staff/sergi-bittan/events-app/src/components/App.js
+++ staff/sergi-bittan/events-app/src/components/App.js
@@ -2,30 +2,19 @@ import React, { useState, useEffect } from 'react'
 import logo from './logo.svg'
 // import './App.css'
 import './App.sass'
-import { sayHello } from '../logic'
+import "./register.sass"
+import  Register  from './register'
 
 function App({ name }) {
-  const [count, setCount] = useState(0)
-  const [view, setView] = useState('home')
-  const [hello, setHello] = useState()
+  
+  const [view, setView] = useState(undefined)
+  
+  
+  return <Register></Register>
 
-  function countUp(event) {
-    event.preventDefault()
+  
 
-    setCount(count + 1)
-    count > 4 && setView('message')
-  }
-
-  useEffect(() => { sayHello(name).then(setHello) }, [])
-
-  return <div className="App">
-    <h1>{hello}</h1>
-    <form onSubmit={countUp}>
-      <span>{count}</span>
-      {view === 'message' && <h2>count {count} reached!</h2>}
-      <button>++</button>
-    </form>
-  </div>
+  
 }
 
 export default App
--- staff/sergi-bittan/events-app/src/components/register.js
+++ staff/sergi-bittan/events-app/src/components/register.js
@@ -1,7 +1,7 @@
 import React from "react"
 export default
 
-function Register() {
+function Register({onRegister, onToLogin}) {
     return <form className="register" onSubmit={event => {
         event.preventDefault()
 
